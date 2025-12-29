87f47a1bd798471a5774c6b2fcd99ead9cdcc44f
NastifyMe <suprunar2010@gmail.com>
2025-12-29 12:05:58 +0100

    dasd

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
-
 import fetchCountries from './fetchCountries.js';
 import { listCountry, countryCard } from './templates.js';
 
+const inputCountry = document.querySelector('.inputCountry');
+const container = document.querySelector('.container');
 
+function debounce(fn, delay) {
+  let timeoutId;
+  return function(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), delay);
+  }
+}
 
-const inputCountry = document.querySelector('.inputCountry')
-const container = document.querySelector('.container')
-
-
-const debounceSearch = search
-
-inputCountry.addEventListener('input', debounceSearch)
+const debounceSearch = debounce(search, 500);
+inputCountry.addEventListener('input', debounceSearch);
 
 function search(event) {
-    const countryName = event.target.value.trim()
+  const countryName = event.target.value.trim();
 
-    if (!countryName){
-        container.innerHTML = ''
-        return
-    }
+  if (!countryName) {
+    container.innerHTML = '';
+    return;
+  }
 
-    fetchCountries(countryName)
-        .then(countries => {
-            container.innerHTML = ''
+  fetchCountries(countryName)
+    .then(countries => {
+      container.innerHTML = '';
 
-            if (countries.length > 10) {
-                console.log('Зробіть запит більш специфічним')
-                return
-              }
+      if (!countries || countries.length === 0) {
+        alert('Країну не знайдено');
+        return;
+      }
 
-            if (countries.length >= 2){
-                container.innerHTML = listCountry(countries)
-                return
-            }
+      if (countries.length > 10) {
+        alert('Зробіть запит більш специфічним');
+        return;
+      }
 
-            container.innerHTML = countryCard(countries[0])
-        })
+      if (countries.length >= 2) {
+        container.innerHTML = listCountry(countries);
+        return;
+      }
 
-        .catch(() => {
-            error({
-              text: 'Країну не знайдено',
-            });
-          });
-    
+      container.innerHTML = countryCard(countries[0]);
+    })
+    .catch(() => {
+      alert('Країну не знайдено');
+    });
 }
-
 
 function countryClick(event) {
-    if (!event.target.classList.contains('country-item')) return
+  if (!event.target.classList.contains('country-item')) return;
 
-    const countryItem = event.target.dataset.name
-    inputCountry.value = countryItem
+  const countryItem = event.target.dataset.name;
+  inputCountry.value = countryItem;
 
-    fetchCountries(countryItem).then(countries => {
-        container.innerHTML = countryCard(countries[0])
-    })
+  fetchCountries(countryItem).then(countries => {
+    container.innerHTML = countryCard(countries[0]);
+  });
 }
 
-container.addEventListener('click', countryClick)
+container.addEventListener('click', countryClick);
